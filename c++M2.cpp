d3184bb58b737c6d530232e3ebabc8de62013f09
phuong2k4 <phuongsepfg@gmail.com>
2024-03-22 15:42:08 +0700

    .

--- c++M2.cpp
+++ c++M2.cpp
@@ -392,3 +392,37 @@ using namespace std;
 //     }
 //     return 0;
 // }
+
+int gcd(int a,int b){
+    if(b==0)return a;
+    return gcd(b,a%b);
+}
+const int maxn=100001;
+int a[maxn];
+int main(){
+    int t;
+    cin >>t;
+    while(t--){
+        int n;
+        cin >> n;
+        for(int i =0;i<n;i++){
+            cin >> a[i];
+        }
+        int res = 0;
+        for(int i = 0;i<n;i++){
+            res = gcd(res,a[i]);
+        }
+        // if(res == 1) cout << 0 <<endl; continue;
+        int ans = 1;//bang chinh no
+        for(int i =2;i<sqrt(res);i++){
+            if(res % i == 0)
+            {
+                ++ans;
+                if(i != res / i) ++ans;
+            }
+            
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
