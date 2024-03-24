634f3745116257ccccc69c579e1d4f76b6d938ef
phuong2k4 <phuongsepfg@gmail.com>
2024-03-24 18:21:56 +0700

    .

--- c++.cpp
+++ c++.cpp
@@ -5034,4 +5034,32 @@ int main(){
 //         }
 //         cout << endl;
 //     }
+
+/////xau ki tu day du
+
+// int lis(char c[]){
+//     int lis[strlen(c)];
+
+//     for(int i = 0; i < strlen(c);i++){
+//         lis[i]=1;
+//         for(int j=0;j<i;j++){
+//             if(c[i]>c[j] && lis[i] < lis[j]+1){
+//                 lis[i] = lis[j]+1;
+//             }
+//         }
+//     }
+//     int res = 0;
+//     for(int i = 0; i<strlen(c);i++){
+//         if(lis[i]>res){
+//             res = lis[i];
+//         }
+//     }
+//     return 26-res;
+// }
+
+// int main(){
+//     char c[1000];
+//     gets(c);
+//     cout << lis(c);
+//     return 0;
 // }
