a215676f440b1fe728b753cd60d631e228fe997a
phuong2k4 <phuongsepfg@gmail.com>
2024-03-29 10:25:33 +0700

    .

--- c++.cpp
+++ c++.cpp
@@ -5222,3 +5222,27 @@ int main(){
 //     }
 //     return 0;
 // }
+
+int solution(char c[]){
+    int res = 1;
+    int cnt = 1;
+    for(int i=0;i<strlen(c)-1;i++){
+        if(c[i]!=c[i+1]){
+            ++cnt;
+        }else{
+            cnt = 1;
+        }
+        res = max(res,cnt);
+    }
+    return res;
+}
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        char c[1000];
+        cin >> c;
+        cout << solution(c) << endl;
+    }
+}
