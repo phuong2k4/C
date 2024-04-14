8bcaba90ffc2cef22be730d13a0b4b0a9b6c343c
phuong2k4 <phuongsepfg@gmail.com>
2024-04-14 08:27:20 +0700

    :.

--- c++.cpp
+++ c++.cpp
@@ -5671,3 +5671,36 @@ int main(){
 //     if(sum %3==0)return 1;
 //     return 0;
 // }
+
+
+///so nhi phan lon chia het cho 5
+
+int solution(char c[]);
+int main(){
+    int testcase;
+    cin >> testcase;
+    while(testcase--){
+        char c[1000];
+        cin >> c;
+        if(solution(c)){
+            cout << "Yes" << endl;
+        }
+        else{
+            cout << "No" << endl;
+        }
+    }
+    return 0;
+}
+int solution(char c[]){
+    int n = strlen(c);
+    int temp=1, total=0;
+    for(int index = n-1;index >=0;index--){
+        total += (c[index]-'0') * temp;
+        temp *=2;temp%=10;
+        total %=10;
+    }
+    if(total % 5==0){
+        return 1;
+    }
+    return 0;
+}
