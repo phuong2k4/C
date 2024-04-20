d3b0d25d39616f8154846a09d28118dd3c3e2661
phuong2k4 <phuongsepfg@gmail.com>
2024-04-20 17:04:39 +0700

    chanle

--- c++.cpp
+++ c++.cpp
@@ -5830,22 +5830,41 @@ int main(){
 // }
 
 //mang 1 chieu
-int main(){
-    int n,arr[100001];
-    std::cin >> n;
-    for(int index = 0;index < n;index++){
-        std::cin >> arr[index];
-    }
-    int biggest = -1e9-1,smallest =1e9+1;
-    for(int index = 0;index<n;index++){
-        if(biggest < arr[index]){
-            biggest = arr[index];
-        }
-        if (smallest > arr[index])
-        {
-            smallest = arr[index];
-        }
-    }
-    std::cout << biggest << " "<< smallest << std::endl;
+// int main(){
+//     int n,arr[100001];
+//     std::cin >> n;
+//     for(int index = 0;index < n;index++){
+//         std::cin >> arr[index];
+//     }
+//     int biggest = -1e9-1,smallest =1e9+1;
+//     for(int index = 0;index<n;index++){
+//         if(biggest < arr[index]){
+//             biggest = arr[index];
+//         }
+//         if (smallest > arr[index])
+//         {
+//             smallest = arr[index];
+//         }
+//     }
+//     std::cout << biggest << " "<< smallest << std::endl;
 
+// }
+
+int main(){
+    int n,arr[10001];
+    cin >> n;
+    for(int index = 0; index < n;index++){
+        cin >> arr[index];
+    }
+    int chan = 0, le = 0;
+    for(int index = 0;index < n;index++){
+        if(index % 2 == 0){
+            le++;
+        }
+        else{
+            chan++;
+        }
+    }
+    cout << chan <<" "<< le;
+    return 0;
 }
