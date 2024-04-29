ffa23a02199248723032ca002043d5f87be742be
phuong2k4 <phuongsepfg@gmail.com>
2024-04-29 17:28:32 +0700

    TichLonNhat

--- c++.cpp
+++ c++.cpp
@@ -6152,3 +6152,32 @@ int main(){
 //     cout << cnt;
 //     return 0;
 // }
+
+
+//tich lon nhat cua hai so trong mang
+int main(){
+    int n, a[10001];
+    cin >> n;
+    for(int i = 0 ;i < n ;i++){
+        cin >> a[i];
+    }
+    int max1 = INT_MIN, max2 = INT_MIN , min1 = INT_MAX, min2 = INT_MAX;
+    for(int i = 0;i<n;i++) {
+        if(a[i] > max1){
+            max2 = max1;
+            max1 = a[i];
+
+        }else if(a[i] > max2){
+            max2 = a[i];
+        }
+        if(a[i] < min1){
+            min2 = min1;
+            min1 = a[i];
+        }else if (a[i] < min2){
+            min2 = a[i];
+        }
+    }
+    long long res = max(1ll * max1 * max2,1ll* min1 * min2);
+    cout << res;
+    return 0;
+}
