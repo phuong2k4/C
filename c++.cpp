be3ad082d13a7e13f5d50015e5fee6d9479f7db4
phuong2k4 <phuongsepfg@gmail.com>
2024-05-07 03:47:54 +0700

    slidingWindows

--- c++.cpp
+++ c++.cpp
@@ -6420,3 +6420,51 @@ int main(){
 //     cout << sum2 << endl;
 //     return 0; 
 // }
+
+
+////sliding Windows
+int main(){
+    int n, k;
+    cin >> n >> k;
+    int a[n];
+    for(int i = 0;i < n;i++){
+        cin >> a[i];
+    }
+    ///cach 1
+    // long long res = -1e18;
+    // int ind;
+    // for(int i = 0;i<=n-k;i++){
+    //     int sum = 0;
+    //     for(int j = 0;j<k;j++){
+    //         sum+=a[i+j];
+    //         if(sum >=res){
+    //             res = sum;
+    //             ind = i;
+    //         }
+    //     }
+    // }
+    // cout << res << endl;
+    // for(int i = 0;i<k;i++){
+    //     cout << a[i+ind] << " ";
+    // }
+
+    //cach2
+    long long res = 0,ind = 0;
+    long long sum = 0;
+    for(int i = 0;i<k;i++){
+        sum += a[i];
+    }
+    res = sum;
+    for(int i = 1 ;i < n-k+1;i++){
+        sum = sum - a[i-1] + a[i+k-1];
+        if (sum >= res){
+            res = sum;
+            ind = i;
+        }
+    }
+    cout << res << endl;
+    for(int i = 0;i<k;i++){
+        cout << a[i+ind] << " ";
+    }
+    return 0; 
+}
