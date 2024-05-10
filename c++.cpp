e1982c136e17f5586fe0e765ba75cf77f702c904
phuong2k4 <phuongsepfg@gmail.com>
2024-05-10 17:18:26 +0700

    GiaoVaHop

--- c++.cpp
+++ c++.cpp
@@ -6560,3 +6560,46 @@ int main(){
 //     }
 // return 0;
 // }
+
+
+///giao va hop cua 2 mang da sap xep
+int min(int a,int b){
+    return a > b? b : a;
+}
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    int a[n], b[m];
+    for(int i = 0;i<n;i++) cin >> a[i];
+    for(int i = 0;i<m;i++) cin >> b[i];
+    int giao[min(n,m)], hop[n+m];
+    int g = 0,h=0,i=0,j=0;
+    while(i<n &&j<m){
+        if(a[i]==b[j]){
+            giao[g] = a[i];
+            hop[h] = a[i];
+            g++;h++;
+            i++;j++;
+        }
+        else if(a[i] > b[j]){
+            hop[h++] = b[j++];
+        }else{
+            hop[h++] = a[i++];
+        }
+    }
+    while(i<n){
+        hop[h++] = a[i++];
+    }
+    while(j<m){
+        hop[h++] = b[j++];
+    }
+    for(int i = 0;i<h;i++){
+        cout << hop[i] << " ";
+    }
+    cout << endl;
+    for(int i = 0;i<g;i++){
+        cout << giao[i] << " ";
+    }
+    return 0;
+}
