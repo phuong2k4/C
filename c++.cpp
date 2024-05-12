1e556efbead044947f8a32e1b67079ea79ca00f0
phuong2k4 <phuongsepfg@gmail.com>
2024-05-12 21:26:33 +0700

    GiaoVaHopEnd

--- c++.cpp
+++ c++.cpp
@@ -6563,72 +6563,74 @@ int main(){
 
 
 ///giao va hop cua 2 mang da sap xep
-int min(int a,int b){
-    return a > b? b : a;
-}
+// int min(int a,int b){
+//     return a > b? b : a;
+// }
 
-int main(){
-    int n, m;
-    cin >> n >> m;
-    int a[n], b[m];
-    for(int i = 0;i<n;i++) cin >> a[i];
-    for(int i = 0;i<m;i++) cin >> b[i];
-    int giao[min(n,m)], hop[n+m];
-    int g = 0,h=0,i=0,j=0;
-    while(i<n &&j<m){
-        if(a[i]==b[j]){
-            giao[g] = a[i];
-            hop[h] = a[i];
-            g++;h++;
-            i++;j++;
-        }
-        else if(a[i] > b[j]){
-            hop[h++] = b[j++];
-        }else{
-            hop[h++] = a[i++];
-        }
-    }
-    while(i<n){
-        hop[h++] = a[i++];
-    }
-    while(j<m){
-        hop[h++] = b[j++];
-    }
-    for(int i = 0;i<h;i++){
-        cout << hop[i] << " ";
-    }
-    cout << endl;
-    for(int i = 0;i<g;i++){
-        cout << giao[i] << " ";
-    }
-    return 0;
-}
+// int main(){
+//     int n, m;
+//     cin >> n >> m;
+//     int a[n], b[m];
+//     for(int i = 0;i<n;i++) cin >> a[i];
+//     for(int i = 0;i<m;i++) cin >> b[i];
+//     int giao[min(n,m)], hop[n+m];
+//     int g = 0,h=0,i=0,j=0;
+//     while(i<n &&j<m){
+//         if(a[i]==b[j]){
+//             giao[g] = a[i];
+//             hop[h] = a[i];
+//             g++;h++;
+//             i++;j++;
+//         }
+//         else if(a[i] > b[j]){
+//             hop[h++] = b[j++];
+//         }else{
+//             hop[h++] = a[i++];
+//         }
+//     }
+//     while(i<n){
+//         hop[h++] = a[i++];
+//     }
+//     while(j<m){
+//         hop[h++] = b[j++];
+//     }
+//     for(int i = 0;i<h;i++){
+//         cout << hop[i] << " ";
+//     }
+//     cout << endl;
+//     for(int i = 0;i<g;i++){
+//         cout << giao[i] << " ";
+//     }
+//     return 0;
+// }
 
-//// giao va hop cua 2 mang chua sap xep
+// //// giao va hop cua 2 mang chua sap xep
 
-int cnt[100001] = {0};
-int main(){
-    int n, m;
-    cin >> n >> m;
-    int a[n], b[m];
-    int min_val= 1e9, max_val = -1e9;
-    for(int i = 0;i<n;i++){
-        cin >> a[i];
-        cnt[a[i]]++;
-        min_val = min(min_val,a[i]);
-        max_val = max(max_val,a[i]);
-    }
-    for(int i = 0;i<m;i++){
-        cin >> b[i];
-        cnt[b[i]]++;
-        min_val = min(min_val,b[i]);
-        max_val = max(max_val,b[i]);
-    }
-    for(int i = min_val; i<= max_val;i++){
-        if(cnt[i]) cout << i << " ";
-    }
-    cout << endl;
-    for(int i = min_val; i <= max_val;i++){
-        if(cnt[i]==2) cout << i << " ";
-    }
-}
+// int cnt[100001] = {0};
+// int main(){
+//     int n, m;
+//     cin >> n >> m;
+//     int a[n], b[m];
+//     int min_val= 1e9, max_val = -1e9;
+//     for(int i = 0;i<n;i++){
+//         cin >> a[i];
+//         cnt[a[i]]++;
+//         min_val = min(min_val,a[i]);
+//         max_val = max(max_val,a[i]);
+//     }
+//     for(int i = 0;i<m;i++){
+//         cin >> b[i];
+//         cnt[b[i]]++;
+//         min_val = min(min_val,b[i]);
+//         max_val = max(max_val,b[i]);
+//     }
+//     for(int i = min_val; i<= max_val;i++){
+//         if(cnt[i]) cout << i << " ";
+//     }
+//     cout << endl;
+//     for(int i = min_val; i <= max_val;i++){
+//         if(cnt[i]==2) cout << i << " ";
+//     }
+// }
+
+
