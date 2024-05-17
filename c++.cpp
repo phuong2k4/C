03e1dbe9b48261187f1cd53af64637ed4f72629d
phuong2k4 <phuongsepfg@gmail.com>
2024-05-18 04:41:03 +0700

    endDoitien

--- c++.cpp
+++ c++.cpp
@@ -6735,19 +6735,22 @@ int main(){
 
 
 /////thuat toan tham lam // Doi tien
-int main(){
-    int t;
-    cin >> t;
-    while(t--){
-        int n;
-        cin >> n;
-        int a[10] = {1000,500,200,100,50,20,10,5,2,1};
-        int cnt = 0;
-        for(int i = 0 ;i<10;i++){
-            cnt += n / a[i];
-            n %= a[i];
-        }
-        cout << cnt << endl;
-    }
-    return 0;
-}
+// int main(){
+//     int t;
+//     cin >> t;
+//     while(t--){
+//         int n;
+//         cin >> n;
+//         int a[10] = {1000,500,200,100,50,20,10,5,2,1};
+//         int cnt = 0;
+//         for(int i = 0 ;i<10;i++){
+//             cnt += n / a[i];
+//             n %= a[i];
+//         }
+//         cout << cnt << endl;
+//     }
+//     return 0;
+// }
+
+
+//
