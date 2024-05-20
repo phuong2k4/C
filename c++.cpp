4ac61b01319ac06e5692f920004d18005f92f948
DAVID <130499224+phuong2k4@users.noreply.github.com>
2024-05-21 04:41:29 +0700

    updatemindset

--- c++.cpp
+++ c++.cpp
@@ -6832,24 +6832,22 @@ int main(){
 
 //a^2 + b^2 = c^2 
 //tim ra 3 so tao nen 3 canh cua tam giac vuong
-void SelectionSort(long long a[],int n){
-    for(int i = 0;i<n;i++){
+void SelectionSort(int a[],int n){
+    for(int i = 0 ;i<n;i++){
         int min = i;
-        for(int j = i+1;j<n;j++){
-            if(a[j] < a[min]){
-                min = j;
-            }
+        for(int j  = i+1;j<n;j++){
+            if(a[j] < a[min]) min = j;
         }
-        ll flag = a[min];
-        a[min] = a[i];
-        a[i] = flag;
+        int flag = a[i];
+        a[i] = a[min];
+        a[min] = flag;
     }
 }
 
-int check(long long a[],int n){
-    for(int i = n-1;i>=2;i--){
-        int l = 0, r = i-1;
-        while(l<r){
+int check(int a[],int n){
+    for(int i = n-1;i>2;i++){
+        int l = 0, r =n-1;
+        while(l<r) {
             if(a[l] + a[r] == a[i])return 1;
             else if(a[l] + a[r] < a[i]) ++l;
             else --r;
@@ -6864,15 +6862,16 @@ int main(){
     while(t--){
         int n;
         cin >> n;
-        long long a[n];
-        for(int i = 0 ;i<n;i++){
+        int a[n];
+        for(int i = 0;i<n;i++){
             int x;
             cin >> x;
-            a[i] = 1ll* x*x;
+            a[i] = 1ll * x * x;
         }
-        SelectionSort(a,n); 
-        if(check(a,n)) cout <<"Yes" << endl;
-        else cout << "No" << endl;
+        SelectionSort(a,n);
+        if(check(a,n)){
+            cout << "Yes" << endl;
+        }else cout << "No" << endl;
     }
     return 0;
 }
