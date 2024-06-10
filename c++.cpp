0d7fa8d2b55c55205be70ef89cb76d124e5c1886
phuong2k4 <phuongsepfg@gmail.com>
2024-06-11 02:31:44 +0700

    MatranChuyenVi

--- c++.cpp
+++ c++.cpp
@@ -7249,3 +7249,50 @@ int main(){
 //     cout << count;
 //     return 0;
 // }
+
+
+//in mang 2 chieu theo nhieu cach khac nhau
+
+int main(){
+    int n;
+    cin >> n;
+    int a[n][n];
+    for(int i = 0;i<n;i++){
+        for(int j =0;j<n;j++){
+            cin >> a[i][j];
+        }
+    }
+    cout << endl;
+    //ma tran chuyen vi 1
+    for(int i = 0;i<n;i++){
+        for(int j= 0;j<n;j++){
+            cout << a[j][i] << " ";
+        }
+    cout << endl;
+    }
+    cout << endl;
+    //ma tran chuyen vi 2
+    for(int i = n-1;i>=0;i--){
+        for(int j = n-1;j>=0;j--){
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    //ma tran chuyen vi 3
+    for(int i = n-1;i>=0;i--){
+        for(int j = n-1;j>=0;j--){
+            cout << a[j][i] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    //ma tran chuyen vi 4
+    for(int i = n-1;i>=0;i--){
+        for(int j = 0;j<n;j++){
+            cout << a[j][i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
