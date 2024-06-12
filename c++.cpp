8d3fc4c39087fbdcd9f8d372c604ea9ab852156c
phuong2k4 <phuongsepfg@gmail.com>
2024-06-12 18:15:15 -0400

    MaTranXoayOc

--- c++.cpp
+++ c++.cpp
@@ -7418,3 +7418,211 @@ int main(){
 //     return 0;
 // }
 //end 4h26 12/6
+
+
+// ma tran xoay oc
+
+int main(){
+    int n;
+    cin >> n;
+    int a[n][n], cnt = 1;
+    int h1 = 0, h2=n-1,c1=0,c2=n-1;
+    while(h1<=h2&&c1<=c2){
+        for(int i = c1;i<=c2;i++){
+            a[h1][i]=cnt;
+            ++cnt;
+        }
+        ++h1;
+        for(int i = h1;i<=h2;i++){
+            a[i][c2]=cnt;
+            ++cnt;
+        }
+        --c2;
+        if (c1<=c2){
+            for(int i = c2;i>=c1;i--){
+                a[h2][i]=cnt;
+                ++cnt;
+            }
+            --h2;
+        }
+        if (h1<=h2){
+            for(int i = h2;i>=h1;i--){
+                a[i][c1]=cnt;
+                ++cnt;
+            }
+            ++c1;
+        }
+        
+    }
+    for(int i = 0;i<n;i++){
+        for(int j = 0;j<n;j++){
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+// ma tran xoay oc nguoc
+
+int main(){
+    int n;
+    cin >> n;
+    int a[n][n], cnt = n*n;
+    int h1 = 0, h2=n-1,c1=0,c2=n-1;
+    while(h1<=h2&&c1<=c2){
+        for(int i = c1;i<=c2;i++){
+            a[h1][i]=cnt;
+            --cnt;
+        }
+        ++h1;
+        for(int i = h1;i<=h2;i++){
+            a[i][c2]=cnt;
+            --cnt;
+        }
+        --c2;
+        if (c1<=c2){
+            for(int i = c2;i>=c1;i--){
+                a[h2][i]=cnt;
+                --cnt;
+            }
+            --h2;
+        }
+        if (h1<=h2){
+            for(int i = h2;i>=h1;i--){
+                a[i][c1]=cnt;
+                --cnt;
+            }
+            ++c1;
+        }
+        
+    }
+    for(int i = 0;i<n;i++){
+        for(int j = 0;j<n;j++){
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+// ma tran xoay oc nguyen to
+
+int check(int n){
+    for(int i=2;i<=sqrt(n);i++){
+        if(n%i==0){
+            return 0;
+        }
+    }
+    return n>1;
+}
+
+int p[100];
+
+void sang(){
+    int i = 0,cnt = 0;
+    while(cnt<100){
+        if(check(i)){
+            p[cnt] = i;
+            cnt++;
+        }
+        ++i;
+    }
+}
+
+
+int main(){
+    sang();
+    int n;
+    cin >> n;
+    int a[n][n], cnt = 0;
+    int h1 = 0, h2=n-1,c1=0,c2=n-1;
+    while(h1<=h2&&c1<=c2){
+        for(int i = c1;i<=c2;i++){
+            a[h1][i]= p[cnt];
+            ++cnt;
+        }
+        ++h1;
+        for(int i = h1;i<=h2;i++){
+            a[i][c2]=p[cnt];
+            ++cnt;
+        }
+        --c2;
+        if (c1<=c2){
+            for(int i = c2;i>=c1;i--){
+                a[h2][i]=p[cnt];
+                ++cnt;
+            }
+            --h2;
+        }
+        if (h1<=h2){
+            for(int i = h2;i>=h1;i--){
+                a[i][c1]=p[cnt];
+                ++cnt;
+            }
+            ++c1;
+        }
+        
+    }
+    for(int i = 0;i<n;i++){
+        for(int j = 0;j<n;j++){
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+//ma tran xoay oc fibonacci
+
+long long fibo[100];
+
+void fibonacci(){
+    fibo[0]=0;
+    fibo[1]=1;
+    for(int i = 2;i<100;i++){
+        fibo[i] = fibo[i-1] + fibo[i-2];
+    }
+}
+
+int main(){
+    fibonacci();
+    int n;
+    cin >> n;
+    int a[n][n], cnt = 0;
+    int h1 = 0, h2=n-1,c1=0,c2=n-1;
+    while(h1<=h2&&c1<=c2){
+        for(int i = c1;i<=c2;i++){
+            a[h1][i]= fibo[cnt];
+            ++cnt;
+        }
+        ++h1;
+        for(int i = h1;i<=h2;i++){
+            a[i][c2]=fibo[cnt];
+            ++cnt;
+        }
+        --c2;
+        if (c1<=c2){
+            for(int i = c2;i>=c1;i--){
+                a[h2][i]=fibo[cnt];
+                ++cnt;
+            }
+            --h2;
+        }
+        if (h1<=h2){
+            for(int i = h2;i>=h1;i--){
+                a[i][c1]=fibo[cnt];
+                ++cnt;
+            }
+            ++c1;
+        }
+        
+    }
+    for(int i = 0;i<n;i++){
+        for(int j = 0;j<n;j++){
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
