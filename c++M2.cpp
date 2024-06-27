eea2e8e6d481b1b6e50acb786b830bf1e794e74c
phuong2k4 <phuongsepfg@gmail.com>
2024-06-27 12:57:40 -0400

    SumPrefix

--- c++M2.cpp
+++ c++M2.cpp
@@ -688,90 +688,132 @@ using namespace std;
 
 
 // mang 2 chieu 
-int main(){
-    int n,m;
-    cin >> n >> m;
-    char a[n][m];
-    for(int i = 0;i<n;i++){
-        for(int j =0;j<m;j++){
-            cin >> a[i][j];
-        }
-    }
-    for(int i = 0;i<n;i++){
-        for(int j=0;j<m;j++){
-            cout << a[i][j] << " ";
-        }
-        cout << endl;
-    }
-    return 0;
-}
+// int main(){
+//     int n,m;
+//     cin >> n >> m;
+//     char a[n][m];
+//     for(int i = 0;i<n;i++){
+//         for(int j =0;j<m;j++){
+//             cin >> a[i][j];
+//         }
+//     }
+//     for(int i = 0;i<n;i++){
+//         for(int j=0;j<m;j++){
+//             cout << a[i][j] << " ";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
 
 
 
-// vector
-// nhap mang 2 chieu voi moi dong co so luong phan tu khac nhau
+// // vector
+// // nhap mang 2 chieu voi moi dong co so luong phan tu khac nhau
 
-int main(){
-    int n;
-    cin >> n;
-    vector<int> a[n];
-    for(int i =0;i<n;i++){
-        int m;
-        cin >> m;
-        for(int j =0;j<m;j++){
-            int x;
-            cin >> x;
-            a[i].push_back(x);
-        }
-    }
-    cout << endl; 
-    for (int i =0;i<n;i++){
-        for(int j = 0;j<a[i].size();j++){
-            cout << a[i][j] << " ";
-        }
-        cout << endl;
-    }
-    return 0;
-}
+// int main(){
+//     int n;
+//     cin >> n;
+//     vector<int> a[n];
+//     for(int i =0;i<n;i++){
+//         int m;
+//         cin >> m;
+//         for(int j =0;j<m;j++){
+//             int x;
+//             cin >> x;
+//             a[i].push_back(x);
+//         }
+//     }
+//     cout << endl; 
+//     for (int i =0;i<n;i++){
+//         for(int j = 0;j<a[i].size();j++){
+//             cout << a[i][j] << " ";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
 
-// string 
-// nhap mot chuoi voi so luong phan tu moi dong khac nhau
+// // string 
+// // nhap mot chuoi voi so luong phan tu moi dong khac nhau
 
-int main(){
-    int n;
-    cin >> n;
-    string s[n];
-    cin.ignore();
-    for(int i =0;i<n;i++){
-        getline(cin,s[i]);
-    }
-    for(int i=0;i<n;i++){
-        cout << s[i]  << " ";
-        cout << endl;
-    }
+// int main(){
+//     int n;
+//     cin >> n;
+//     string s[n];
+//     cin.ignore();
+//     for(int i =0;i<n;i++){
+//         getline(cin,s[i]);
+//     }
+//     for(int i=0;i<n;i++){
+//         cout << s[i]  << " ";
+//         cout << endl;
+//     }
     
-    cout << s[2];
-}
+//     cout << s[2];
+// }
 
 
 
-// bai tap coding 
+// // bai tap coding 
 
+// int main(){
+//     int n,m;
+//     cin >> n >> m;
+//     int a[n][m];
+//     for(int i =0;i<n;i++){
+//         for(int j=0;j<m;j++){
+//             cin >> a[i][j];
+//         }
+//     }
+
+//     for(int i=0;i<m;i++){ //chi so hang
+//         for(int j=0;j<n;j++){ // chi so cot
+//             cout << a[j][i] << " ";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+
+
+// tong mang tu l den r
+// prefix aglorithm
 int main(){
-    int n,m;
-    cin >> n >> m;
-    int a[n][m];
-    for(int i =0;i<n;i++){
-        for(int j=0;j<m;j++){
-            cin >> a[i][j];
-        }
+    int n;
+    cin >> n;
+    int a[n];
+    for(int &c : a){
+        cin >> c;
     }
-
-    for(int i=0;i<m;i++){ //chi so hang
-        for(int j=0;j<n;j++){ // chi so cot
-            cout << a[j][i] << " ";
+    int q;
+    cin >> q;
+    while(q--){
+        int l,r,sum = 0;
+        cin >> l >> r;
+        for(int i=l;i<r;i++){
+            sum += a[i];
         }
-        cout << endl;
+        cout << sum;
     }
     return 0;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int a[n+1];
+    for(int i =0;i<n;i++ ) cin >> a[i];
+    int prefix[n+1] = {0};
+    for(int i=1;i<=n;i++){
+        prefix[i] = prefix[i-1] + a[i]; 
+    }
+    int q;
+    cin >> q;
+    while(q--){
+        int l,r; 
+        cin >> l >> r;
+        cout << prefix[r] - prefix[l-1] << endl;
+    }
 }
