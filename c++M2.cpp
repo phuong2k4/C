6aa10fb683261ac73c4b0e30b8ec3bd2d730b830
phuong2k4 <phuongsepfg@gmail.com>
2024-07-02 00:12:07 -0400

    SumPrefix1and2Chieu

--- c++M2.cpp
+++ c++M2.cpp
@@ -780,6 +780,8 @@ using namespace std;
 
 // tong mang tu l den r
 // prefix aglorithm
+
+// mang 1 chieu
 int main(){
     int n;
     cin >> n;
@@ -816,4 +818,57 @@ int main(){
         cin >> l >> r;
         cout << prefix[r] - prefix[l-1] << endl;
     }
+}
+
+
+// mang 2 chieu
+int main(){
+    int n,m;
+    cin >> n >> m;
+    int a[n][m];
+    for(int i =0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin >> a[i][j];
+        }
+    }
+    int q;
+    cin >> q;
+    while(q--){
+        int hn,hm,cn,cm;
+        cin >> hn >> hm >> cn >> cm;
+        int sum =0;
+        for(int i=hn;i<=hm;i++){
+            for(int j = cn;j<=cm;j++){
+                sum += a[i][j];
+            }
+            cout << sum << endl;
+        }
+    }
+    return 0;
+} 
+
+
+int main(){
+    int n,m;
+    cin >> n >> m;
+    int a[n+1][m+1];
+    for(int i =1;i<=n;i++){
+        for(int j =1;j<=m;j++){
+            cin >> a[i][j];
+        }
+    }
+    int pre[n+1][m+1] = {0};
+    for(int i =1;i<=n;i++){
+        for(int j =1;j<=m;j++){
+            pre[i][j] = pre[i-1][j] + pre[i][j-1] - pre[i-1][j-1]+a[i][j];
+        }
+    }
+    int q;
+    cin >> q;
+    while(q--){
+        int h1,h2,c1,c2;
+        cin >> h1 >> h2 >>c1>>c2;
+        cout << pre[h2][c2] - pre[h1-1][c2] - pre[h2][c1-1] + pre[h1-1][c1-1] << endl;
+    }
+    return 0;
 }
